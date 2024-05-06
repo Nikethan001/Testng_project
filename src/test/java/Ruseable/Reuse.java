115ee26478c673a255dce989b15a771f1d79afb0
Nikethanhari <harish.3s@yahoo.com>
2024-05-06 11:27:59 +0530

    Initial commit

---
+++ src/test/java/Ruseable/Reuse.java
@@ -0,0 +1,18 @@
+package Ruseable;
+
+public class Reuse {
+    public String CreatePostJson(String name,String year,String DOB,String Address,String Salary)
+    {
+        String body ="{\n" +
+                "    \"name\":\""+name+"\",\n" +
+                "    \"data\":{\n" +
+                "        \"year\": "+year+",\n" +
+                "        \"DOB\": \""+DOB+"\",\n" +
+                "        \"Address\": \""+Address+"\",\n" +
+                "        \"Salary\": \""+Salary+"\"\n" +
+                "    }\n" +
+                "}";
+        return body;
+    }
+
+}
